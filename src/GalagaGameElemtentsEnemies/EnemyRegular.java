8deefe219b77a339329db9bf80455c44af0946ba
josebesed004 <josebesed004@gmail.com>
2025-11-02 00:19:53 -0400

    Add Galaga game elements including Ball, Paddle, and Enemy classes with associated images. Some method stubs so that my teammates can start working on their parts while having a basis of what I am going to implement in the future

---
+++ src/GalagaGameElemtentsEnemies/EnemyRegular.java
@@ -0,0 +1,13 @@
+package GalagaGameElemtentsEnemies;
+
+public class EnemyRegular extends EnemyBase {
+	
+	String imagePath = "/breakout-the-scapers/src/Pictures/Enemies/NormalEnemy.png";
+	
+	public EnemyRegular(double x, double y, double width, double height, int hp) {
+		super(x, y, width, height, hp);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+}
